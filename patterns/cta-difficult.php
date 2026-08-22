aab19cfad365eb2a9579c6767daead806e2cf469
imaeonic <107129482+imaeonic@users.noreply.github.com>
2026-08-22 15:19:00 +0100

    feat: add high-impact editable CTA pattern

---
+++ patterns/cta-difficult.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Title: Difficult problem CTA
+ * Slug: rismor/cta-difficult
+ * Categories: rismor-conversion
+ * Description: High-contrast closing call to action.
+ * Viewport Width: 1440
+ */
+?>
+<!-- wp:group {"align":"full","backgroundColor":"mist","style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-mist-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"fontSize":"3xl"} --><h2 class="wp-block-heading has-3-xl-font-size">Got something difficult?<br><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-blue-color">Good.</mark></h2><!-- /wp:heading -->
+<!-- wp:group {"layout":{"type":"default"}} --><div class="wp-block-group"><!-- wp:paragraph --><p>Tell us what you are trying to achieve. We will work out the technology around it.</p><!-- /wp:paragraph --><!-- wp:buttons --><div class="wp-block-buttons"><!-- wp:button {"className":"is-style-rismor-arrow"} --><div class="wp-block-button is-style-rismor-arrow"><a class="wp-block-button__link wp-element-button" href="/contact/">Talk to Rismor</a></div><!-- /wp:button --></div><!-- /wp:buttons --></div><!-- /wp:group --></div><!-- /wp:group --></div><!-- /wp:group -->
